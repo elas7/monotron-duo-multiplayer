f46e37fffeb36ef97c49d3d572d11e2a8a1bab06
Seba <sebaest77@gmail.com>
2016-05-02 06:41:17 -0300

    chore(build): use webpack through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const sass = require('gulp-sass');
+const webpack = require("webpack");
 
+/**
+ * SASS tasks
+ */
 const SCSS_SRC_FOLDER = path.resolve(__dirname, 'app/scss/');
 const SCSS_SRC_FILES = path.resolve(__dirname, SCSS_SRC_FOLDER, '**/*.scss');
 const SCSS_SRC_FILE = path.resolve(__dirname, SCSS_SRC_FOLDER, 'main.scss');
@@ -18,4 +23,41 @@ gulp.task('build:watch:scss', ['build:scss'], function() {
   gulp.watch(SCSS_SRC_FILES, ['build:scss']);
 });
 
-gulp.task('default', ['build:watch:scss']);
+/**
+ * Webpack tasks
+ */
+const compiler = webpack(require("./webpack.config"));
+
+const printReport = function(stats) {
+  gutil.log('[webpack]', stats.toString({
+    modules: false,
+    errorDetails: false,
+    timings: false,
+    cached: false,
+    colors: true
+  }));
+};
+
+gulp.task('build:webpack', function() {
+  compiler.run(function(err, stats) {
+    if(err) {
+      gutil.log('error', new gutil.PluginError('[webpack]', err));
+    }
+
+    printReport(stats);
+  });
+});
+
+gulp.task('build:watch:webpack', function() {
+  compiler.watch({
+    aggregateTimeout: 300
+  }, function(err, stats) {
+    if(err) {
+      gutil.log('error', new gutil.PluginError('[webpack]', err));
+    }
+
+    printReport(stats);
+  });
+});
+
+gulp.task('default', ['build:watch:scss', 'build:watch:webpack']);
